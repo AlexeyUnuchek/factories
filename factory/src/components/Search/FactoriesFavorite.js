1f5f4489b5555f11866fc5b507391160750e9c88
AlexeyUnuchek <alexeyunuchek@gmail.com>
2023-12-27 23:05:59 +0200

    update css,js

--- factory/src/components/Search/FactoriesFavorite.js
+++ factory/src/components/Search/FactoriesFavorite.js
@@ -2,14 +2,21 @@ import React from 'react'
 import './searchFavorite.css'
 
 export const FactoriesFavorite = () => {
+    const factories = [
+        {name: "Tnuva"}, {name: "Danon"}, {name: "Nestle"}
+    ]
+
     return (
         <div className='favorite-conteiner'>
             <div className='search1'>Search by product or key words</div>
             <div className='row1'>Filter:  Industry  Country</div>
             <div className='grid-conteiner1'>
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
+                { factories.map((item) => (
+                    <div>{item.name}</div>
+                ))}
+                
+                
+                
                 <div>4</div>
                 <div>5</div>
                 <div>6</div>
--- factory/src/components/SideBar.js
+++ factory/src/components/SideBar.js
@@ -1,18 +1,28 @@
+import { Link } from 'react-router-dom';
 import './sidebar.css';
+import { useState } from 'react';
 
 function SideBar() {
+    const [showfactoriesItems, setShowfactoriesItems] = useState(false);
     return (
         <div className="sideBar">
-            <ul>
-                <li>Username</li>
-                <li>Dashboard</li>
-                <li>Factories</li>
-                <li>Agents</li>
-                <li>Business requests</li>
-                <li>Orders</li>
-                <li>Settings</li>
-                <li> Help</li>
-            </ul>
+            <div>
+                <div className='nav-link'><Link to='username'>Username</Link></div>
+                <div><Link to='/'>Dashboard</Link></div>
+                <div className='nav-link' onClick={() => setShowfactoriesItems(!showfactoriesItems)}> Factories</div>
+
+                {showfactoriesItems &&
+                    <div>
+                        <div className='nav-subItem'><Link to='search'>Search</Link></div>
+                        <div className='nav-subItem'><Link to='favorite'>Favorite</Link></div>
+                    </div>
+                }
+                <div ><Link to='agents'>Agents</Link> </div>
+                <div Link to='businessRequest'>Business requests</div>
+                <div>Orders</div>
+                <div>Settings</div>
+                <div> Help</div>
+            </div>
         </div>
     )
 }
